9cb2599e14babe304e84d2b77fcc58a22e2a7a4d
Richard Hull <rm_hull@yahoo.co.uk>
2016-12-10 22:29:43 +0000

    Ping host supplied on cmdline

--- powerdown_watchdog.py
+++ powerdown_watchdog.py
@@ -7,7 +7,9 @@ import os
 import syslog
 import ping
 import socket
+import sys
 import time
+
 
 class PingTest(ping.Ping):
     def __init__(self, *args, **kwargs):
@@ -17,15 +19,16 @@ class PingTest(ping.Ping):
     def print_success(self, delay, ip, packet_size, ip_header, icmp_header):
         if (self.failed_call_count > 0):
             self.failed_call_count = 0
-            syslog.syslog("Ping successful: " + ip + " - reset failed count")
+            syslog.syslog("Ping successful: {0} - reset failed count".format(ip))
 
     def print_failed(self):
         self.failed_call_count += 1
-        syslog.syslog(syslog.LOG_WARNING, "Ping unsuccessful - failed count incremented: " + str(self.failed_call_count))
+        syslog.syslog(syslog.LOG_WARNING, "Ping unsuccessful - failed count incremented: {0}".format(self.failed_call_count))
 
     def print_unknown_host(self):
         self.failed_call_count += 1
-        syslog.syslog(syslog.LOG_WARNING, "Unknown host - failed count incremented: " + str(self.failed_call_count))
+        syslog.syslog(syslog.LOG_WARNING, "Unknown host - failed count incremented: {0}".format(self.failed_call_count))
+
 
 def child(host, max_fails=5, startup_delay=600, interval=60):
     try:
@@ -39,14 +42,15 @@ def child(host, max_fails=5, startup_delay=600, interval=60):
         while True:
             ping.run(count=1)
             if ping.failed_call_count >= max_fails:
-                syslog.syslog(syslog.LOG_ALERT, str(max_fails) + " or more successive pings failed... shutting down")
+                syslog.syslog(syslog.LOG_ALERT, "{0} or more successive pings failed... shutting down".format(max_fails))
                 os.system('shutdown -h now')
 
             time.sleep(interval)
 
-    except socket.error, e:
-        print "Ping Error:", e
+    except socket.error as e:
+        print("Ping Error: {0}".format(e))
+
 
 newpid = os.fork()
 if newpid == 0:
-    child("sannat")
+    child(sys.argv[1])
